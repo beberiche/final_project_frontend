f794d43cc807e435ee71b18493fbe1253b222998
Ojeonghwan <ojh7086@gmail.com>
2022-05-18 16:53:48 +0900

    비디오 리스트,검색 출력

--- src/store/index.js
+++ src/store/index.js
@@ -1,17 +1,36 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
-
-Vue.use(Vuex)
-
+import Vue from "vue";
+import Vuex from "vuex";
+import axios from "axios";
+Vue.use(Vuex);
+//http://localhost:9999/api/video/list
 export default new Vuex.Store({
   state: {
+    videos: [],
   },
-  getters: {
-  },
+  getters: {},
   mutations: {
+    setvideos(state, payload) {
+      state.videos = payload;
+    },
   },
   actions: {
+    getList({ commit }, payload) {
+      let params = null;
+      if (payload) {
+        params = payload;
+      }
+      axios({
+        url: "http://localhost:9999/api/video/list",
+        method: "GET",
+        params,
+      })
+        .then((res) => {
+          commit("setvideos", res.data);
+        })
+        .catch((res) => {
+          console.log(res);
+        });
+    },
   },
-  modules: {
-  }
-})
+  modules: {},
+});
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,17 +1,53 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    
+    <!--<video-one  v-for = "video in videos" :key = "video.youtubeId" :video="video" :size="size"> </video-one> -->
+    <div v-for="(video, index) in videos" :key="index">
+      <video-one :video="video" :size="size" />
+    </div>
+
+    <div>
+      <select v-model="searchdata.key">
+        <option value="1">파트</option>
+        <option value="2">제목</option>
+        <option value="3">채널이름</option>
+      </select>
+      <input type="text" v-model="searchdata.content" />
+      <button @click="search">검색</button>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-
+import { mapState } from "vuex";
+import VideoOne from "@/component/VideoOne.vue";
 export default {
-  name: 'HomeView',
+  name: "VideoList",
+  data() {
+    return {
+      size: {
+        width: 300,
+        height: 300,
+      },
+      searchdata: {
+        key: "",
+        content: "",
+      },
+    };
+  },
+  computed: {
+    ...mapState(["videos"]),
+  },
+  created() {
+    this.$store.dispatch("getList", this.serch);
+  },
+  methods: {
+    search() {
+      this.$store.dispatch("getList", this.searchdata);
+    },
+  },
   components: {
-
-  }
-}
+    VideoOne,
+  },
+};
 </script>
