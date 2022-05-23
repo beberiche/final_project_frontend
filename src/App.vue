a112568f7fd9d50752dc31e0a4129065787a09f3
beberiche <whdgus269@gmail.com>
2022-05-23 13:02:02 +0900

    운동 명언 프로모션 제작

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,34 @@
 <template>
-	<div id="app">
-		<div class="container">
-			<section class="aside-container">
-				<header-nav></header-nav>
-			</section>
-			<section class="view_wrapper">
-				<transition name="fade" mode="out-in">
-					<router-view />
-				</transition>
-			</section>
+	<div class="body">
+		<header-promotion></header-promotion>
+		<div id="app">
+			<div class="container">
+				<section class="aside-container">
+					<header-nav></header-nav>
+				</section>
+				<section class="view_wrapper">
+					<transition name="fade" mode="out-in">
+						<router-view />
+					</transition>
+				</section>
+			</div>
 		</div>
+		<!-- <footer-promotion></footer-promotion> -->
 	</div>
 </template>
 
 <script>
 import HeaderNav from "./components/common/HeaderNav.vue";
+// import FooterPromotion from "./components/promotion/FooterPromotion.vue";
+import HeaderPromotion from "./components/promotion/HeaderPromotion.vue";
 export default {
-	components: { HeaderNav },
+	components: { HeaderNav, HeaderPromotion },
 };
 </script>
 
 <style>
 @import url("https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@100;300;400;500;700;900&display=swap");
+
 #app {
 	font-family: "Noto Sans KR", sans-serif;
 	-webkit-font-smoothing: antialiased;
@@ -30,7 +37,7 @@ export default {
 	display: flex;
 	justify-content: center;
 	align-items: center;
-	height: 100vh;
+	height: 95vh;
 }
 
 .container {
